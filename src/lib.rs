22609f4acd16887223d29b913658402336d031b3
Albert Ford <albert@albertford.com>
2024-08-08 19:53:07 -0700

    Add bevy dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,22 @@
+use bevy_app::App;
+use bevy_log::BoxedLayer;
+
+/// ```
+/// # use bevy_app::{App, PluginGroup};
+/// # use bevy_internal::DefaultPlugins;
+/// # use bevy_log::LogPlugin;
+/// # use bevy_gizmo_log::gizmo_log_layer;
+/// App::new().add_plugins(
+///     DefaultPlugins.set(LogPlugin {
+///         custom_layer: gizmo_log_layer,
+///         ..Default::default()
+///     })
+/// );
+/// ```
+pub fn gizmo_log_layer(_app: &mut App) -> Option<BoxedLayer> {
+    None
+}
+
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
